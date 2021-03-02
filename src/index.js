af4331f4a34720547e9212aa6c0834d8fd5b28e6
Voronko Sergey <vss1502@i.ua>
2021-03-02 14:24:12 +0200

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  module.exports = function check(str, bracketsConfig) {
+    let arr = str.split('');
+   
+    let rules = /\(\)|\|\||\[\]|\{\}|[1][2]|[3][4]|[5][6]|[7][7]|[8][8]/;
+    while(str = str.replace(rules,'')){ 
+      if(arr == 0) return true; 
+      if(str.length == arr) return false;
+      arr = str.length; 
+    }
+    return true;
+  } 
+
+
+
+
+  
